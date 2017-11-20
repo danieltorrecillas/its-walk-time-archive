9111c8c0accc7162b7623db2c6cd1fa3a15e3b96
Daniel Torrecillas <22879920+danieltorrecillas@users.noreply.github.com>
2017-11-20 00:36:41 -0800

    Add authentication, site wide

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: ENV['SERVER_USER_NAME'], password: ENV['SERVER_PASSWORD']
   protect_from_forgery with: :exception
 end
