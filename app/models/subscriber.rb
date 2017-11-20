de26c94dc19bbf85889495bac76f2fb2a6b54757
Daniel Torrecillas <22879920+danieltorrecillas@users.noreply.github.com>
2017-11-20 00:37:38 -0800

    Add validation

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,2 +1,4 @@
 class Subscriber < ApplicationRecord
+  validates :last_name, :first_name, :phone_number, presence: true
+  validates :phone_number, length: { is: 12 }, format: { with: /\+\d{11}/ }, uniqueness: true
 end
